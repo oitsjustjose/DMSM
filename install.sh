f17df39b42e340990a6125e770f1424d8bfe8ec3
oitsjustjose <stovallj1995@gmail.com>
2021-06-11 10:07:14 -0500

    Update install

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ then
     exit 1;
 fi
 
-export url="https://github.com/oitsjustjose/DMSM/releases/download/1.0.0/dmsm"
+export url="https://github.com/oitsjustjose/DMSM/releases/download/1.1.0/dmsm_x86-64"
 
 if [ ! $(command -v docker) ]
 then
